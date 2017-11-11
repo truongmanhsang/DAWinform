3922f1c92f069ab6b77b3a95f17f2c785ee991bb
truongmanhsang <tr.manhsang@gmail.com>
2017-11-12 00:12:43 +0700

    Hoan thien quan ly loai, hang san xuat, update database

--- GUI/UserControls/ucHangSanXuat.cs
+++ GUI/UserControls/ucHangSanXuat.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
+using DTO;
 
 namespace GUI
 {
     public partial class ucHangSanXuat : UserControl
     {
+        clsHangSanXuat_BUS _HangSanXuatBUS = new clsHangSanXuat_BUS();
+
+        DataTable dtHSX;
+
+        DataView dvHSX;
         public ucHangSanXuat()
         {
             InitializeComponent();
+        }
+
+        private void ucHangSanXuat_Load(object sender, EventArgs e)
+        {
+            CaiDat();
+            TaiDuLieu();
+        }
+        private void CaiDat()
+        {
+            dgvHSX.AutoGenerateColumns = false;
+        }
+
+        private void TaiDuLieu()
+        {
+            dtHSX = _HangSanXuatBUS.LayBangHangSanXuat();
+            dvHSX = new DataView(dtHSX);
+            dgvHSX.DataSource = dvHSX;
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            dvHSX.RowFilter = string.Format("TenHangSanXuat like '%{0}%'", txtTenHSX.Text);
+        }
+        private void XuLiThemHSX(clsHangSanXuat_DTO hsx)
+        {
+            if (_HangSanXuatBUS.ThemHSX(hsx))
+            {
+                MessageBox.Show("Thêm hãng sản xuất thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                TaiDuLieu();
+            }
+            else
+            {
+                MessageBox.Show("Thêm hãng sản xuất không thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void XuLiSuaHSX(clsHangSanXuat_DTO hsx)
+        {
+            if (_HangSanXuatBUS.SuaHSX(hsx))
+            {
+                MessageBox.Show("Sửa hãng sản xuất thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                TaiDuLieu();
+            }
+            else
+            {
+                MessageBox.Show("Sửa hãng sản xuất không thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            frmHangSanXuat frm = new frmHangSanXuat();
+            frm.themHSX += XuLiThemHSX;
+            frm.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dgvHSX.SelectedRows[0].Index != -1)
+            {
+                string strMaHSX = dgvHSX.SelectedRows[0].Cells["colMaLoai"].Value.ToString();
+                frmHangSanXuat frm = new frmHangSanXuat(strMaHSX);
+                frm.suaHSX += XuLiSuaHSX;
+                frm.ShowDialog();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dgvHSX.SelectedRows[0].Index != -1)
+            {
+                string strMaHSX = dgvHSX.SelectedRows[0].Cells["colMaLoai"].Value.ToString();
+                if (MessageBox.Show("Bạn chắc chắn muốn xoá hãng sản xuất này?", "Xác nhận xoá", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    if (_HangSanXuatBUS.XoaHSX(strMaHSX))
+                    {
+                        MessageBox.Show("Xoá hãng sản xuất thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        TaiDuLieu();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xoá hãng sản xuất không thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
---
+++ GUI/frmHangSanXuat.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GUI
+{
+    partial class frmHangSanXuat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.txtGhiChu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenHSX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHuy.Location = new System.Drawing.Point(319, 8);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(108, 38);
+            this.btnHuy.TabIndex = 1;
+            this.btnHuy.Text = "Huỷ";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLuu.Location = new System.Drawing.Point(205, 8);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(108, 38);
+            this.btnLuu.TabIndex = 0;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // txtGhiChu
+            // 
+            this.txtGhiChu.Location = new System.Drawing.Point(153, 35);
+            this.txtGhiChu.Name = "txtGhiChu";
+            this.txtGhiChu.Size = new System.Drawing.Size(274, 26);
+            this.txtGhiChu.TabIndex = 88;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 87;
+            this.label2.Text = "Ghi chú";
+            // 
+            // txtTenHSX
+            // 
+            this.txtTenHSX.Location = new System.Drawing.Point(153, 3);
+            this.txtTenHSX.Name = "txtTenHSX";
+            this.txtTenHSX.Size = new System.Drawing.Size(274, 26);
+            this.txtTenHSX.TabIndex = 86;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 18);
+            this.label1.TabIndex = 85;
+            this.label1.Text = "Tên hãng sản xuất";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btnHuy);
+            this.panel1.Controls.Add(this.btnLuu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 54);
+            this.panel1.TabIndex = 84;
+            // 
+            // frmHangSanXuat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 141);
+            this.Controls.Add(this.txtGhiChu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTenHSX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmHangSanXuat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmHangSanXuat";
+            this.Load += new System.EventHandler(this.frmHangSanXuat_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.TextBox txtGhiChu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenHSX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
